9b04e6e6434a152985f60d70eb3133f70d214ef3
vagrant <vagrant@localhost.localdomain>
2020-06-02 06:57:28 +0000

    [start]5-3

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '9d9d90e98f3d1d3e637114c6f0b911d2161835a31fabcb2c029ea1586c100be88a3474a1cb6cdb3abea54a9ceb1ddc89d4813c404f571b3183a7e0f8fcce5561'
---
+++ db/migrate/20200601114903_postlmages.rb
@@ -0,0 +1,5 @@
+class Postlmages < ActiveRecord::Migration[5.2]
+  def change
+  	drop_table :postlmages
+  end
+end
